75e75f56db19491028d8d7c368ad864546f7a983
TIGRICA333 <318662967+TIGRICA333@users.noreply.github.com>
2026-09-16 04:18:58 +0000

    Исправил белый экран: добавил base-путь для GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/biblioteka-cenet-re4i/",
   plugins: [react()],
   server: { host: "0.0.0.0", hmr: false },
 });
